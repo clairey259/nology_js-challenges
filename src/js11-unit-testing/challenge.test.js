89eff880cfc4977f5dd4c45a1d4abeb7d7d63fd9
Claire <claire.huizenga@outlook.com>
2022-09-20 15:07:02 +1000

    finished js 11

--- src/js11-unit-testing/challenge.test.js
+++ src/js11-unit-testing/challenge.test.js
@@ -5,14 +5,29 @@ test("correct greeting", () => {
 })
 
 test("adds two numbers and returns the sum", () => {
-    expect (challenge.sumTwoNumbers(1, 4).toBe(5));
-    expect (challenge.sumTwoNumbers(8, 9).toBe(17));
-    expect (challenge.sumTwoNumbers(-5, 5).toBe(0));
-    expect (challenge.sumTwoNumbers(360, 18).toBe(378));
+    expect (challenge.sumTwoNumbers(1, 4)).toBe(5);
+    expect (challenge.sumTwoNumbers(8, 9)).toBe(17);
+    expect (challenge.sumTwoNumbers(-5, 5)).toBe(0);
+    expect (challenge.sumTwoNumbers(360, 18)).toBe(378);
 })
 
 test("check if one number is larger than the other", () =>{
-    expect (challenge.checkLarger (5, 5).toBe("Both numbers are equal"));
-    expect (challenge.checkLarger (8, 2).toBe(`8 is bigger than 2`));
-    expect (challenge.checkLarger (75, 598).toBe(`598 is bigger than 75`));
+    expect (challenge.checkLarger (5, 5)).toBe("Both numbers are equal");
+    expect (challenge.checkLarger (8, 2)).toBe(`8 is bigger than 2`);
+    expect (challenge.checkLarger (75, 598)).toBe(`598 is bigger than 75`);
 })
+
+describe("array contains valid names", () => {
+    test ("returns valid names", () => {
+        expect (challenge.filterByLength(["claire", "vera", "jordan", "sadlkfjasasdf"])).toStrictEqual(["claire", "vera", "jordan"])
+        expect (challenge.filterByLength(["sadlkfjasasdf", "sadlkfjasasdf", "sadlkfjasasdf"])).toBe("Sorry, no valid names supplied")
+    })
+  
+})
+
+test ("reduces numbers in array by reducer amount", () => {
+    expect (challenge.reduceNumbers([8, 9, 7], 1)).toStrictEqual([7, 8, 6]);
+    expect (challenge.reduceNumbers([300, 9, 60], 10)).toStrictEqual([255, 0, 50]);
+
+})
+
--- src/js11-unit-testing/dist/challenge.test.dev.js
+++ src/js11-unit-testing/dist/challenge.test.dev.js
@@ -12,13 +12,23 @@ test("correct greeting", function () {
   expect(challenge.greet()).toBe("Hello, nice to meet you");
 });
 test("adds two numbers and returns the sum", function () {
-  expect(challenge.sumTwoNumbers(1, 4).toBe(5));
-  expect(challenge.sumTwoNumbers(8, 9).toBe(17));
-  expect(challenge.sumTwoNumbers(-5, 5).toBe(0));
-  expect(challenge.sumTwoNumbers(360, 18).toBe(378));
+  expect(challenge.sumTwoNumbers(1, 4)).toBe(5);
+  expect(challenge.sumTwoNumbers(8, 9)).toBe(17);
+  expect(challenge.sumTwoNumbers(-5, 5)).toBe(0);
+  expect(challenge.sumTwoNumbers(360, 18)).toBe(378);
 });
 test("check if one number is larger than the other", function () {
-  expect(challenge.checkLarger(5, 5).toBe("Both numbers are equal"));
-  expect(challenge.checkLarger(8, 2).toBe("8 is bigger than 2"));
-  expect(challenge.checkLarger(75, 598).toBe("598 is bigger than 75"));
+  expect(challenge.checkLarger(5, 5)).toBe("Both numbers are equal");
+  expect(challenge.checkLarger(8, 2)).toBe("8 is bigger than 2");
+  expect(challenge.checkLarger(75, 598)).toBe("598 is bigger than 75");
+});
+describe("array contains valid names", function () {
+  test("returns valid names", function () {
+    expect(challenge.filterByLength(["claire", "vera", "jordan", "sadlkfjasasdf"])).toStrictEqual(["claire", "vera", "jordan"]);
+    expect(challenge.filterByLength(["sadlkfjasasdf", "sadlkfjasasdf", "sadlkfjasasdf"])).toBe("Sorry, no valid names supplied");
+  });
+});
+test("reduces numbers in array by reducer amount", function () {
+  expect(challenge.reduceNumbers([8, 9, 7], 1)).toStrictEqual([7, 8, 6]);
+  expect(challenge.reduceNumbers([300, 9, 60], 10)).toStrictEqual([255, 0, 50]);
 });
